a0476c07e5b920717de335b853ec4d98f70029cb
Moos Castelijn <mooscastelijn@gmail.com>
2024-12-05 11:26:07 +0100

    just remove autoprexier.

--- vite-frontend/postcss.config.js
+++ vite-frontend/postcss.config.js
@@ -3,7 +3,5 @@ module.exports = {
   plugins: [
     // Only add tailwindcss in development
     process.env.NODE_ENV === 'development' && require('tailwindcss'),
-    // Other PostCSS plugins (like autoprefixer) should be added regardless of environment
-    require('autoprefixer'),
   ].filter(Boolean), // Removes undefined values, so the list remains valid
 };
