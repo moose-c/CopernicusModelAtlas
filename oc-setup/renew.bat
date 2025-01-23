0380457d07d260d57cd32b41d3f259c76ccd83a7
Moos Castelijn <mooscastelijn@gmail.com>
2025-01-23 12:03:57 +0100

    new build

--- oc-setup/renew.bat
+++ oc-setup/renew.bat
@@ -20,14 +20,15 @@ git commit -m "new build"
 git push
 
 cd oc-setup
-@REM rem Delete all OpenShift builds
-@REM oc delete builds --all -n geo-acc-modelatlas
 
-@REM rem Start the frontend build
-@REM oc start-build frontend
+rem Delete all OpenShift builds
+oc delete builds --all -n geo-acc-modelatlas
 
-@REM rem Wait for 1 minute (use timeout instead of sleep)
-@REM call timeout /t 60
+rem Start the frontend build
+oc start-build frontend
 
-@REM rem Set the image for the frontend deployment
-@REM oc set image deployment/frontend frontend=image-registry.openshift-image-registry.svc:5000/geo-acc-modelatlas/frontend:latest -n geo-acc-modelatlas
+rem Wait for 1 minute (use timeout instead of sleep)
+call timeout /t 60
+
+rem Set the image for the frontend deployment
+oc set image deployment/frontend frontend=image-registry.openshift-image-registry.svc:5000/geo-acc-modelatlas/frontend:latest -n geo-acc-modelatlas
