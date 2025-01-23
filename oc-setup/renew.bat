62abe46d483b30accaa862c2b1de4e47d383291d
Moos Castelijn <mooscastelijn@gmail.com>
2025-01-23 12:06:27 +0100

    new build

--- oc-setup/renew.bat
+++ oc-setup/renew.bat
@@ -28,7 +28,8 @@ rem Start the frontend build
 oc start-build frontend
 
 rem Wait for 1 minute (use timeout instead of sleep)
-call timeout /t 60
+call timeout /t 3
 
+echo 'this is reached'
 rem Set the image for the frontend deployment
 oc set image deployment/frontend frontend=image-registry.openshift-image-registry.svc:5000/geo-acc-modelatlas/frontend:latest -n geo-acc-modelatlas
