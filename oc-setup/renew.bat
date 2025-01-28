a463a41d5a9808eed5667c25b3ba916b849a317f
Moos Castelijn <mooscastelijn@gmail.com>
2025-01-28 10:02:19 +0100

    new build

--- oc-setup/renew.bat
+++ oc-setup/renew.bat
@@ -22,4 +22,4 @@ cd oc-setup
 
 oc delete all -l app=frontend
 
-oc new-app registry.access.redhat.com/ubi8/nodejs-16~git@github.com:moose-c/CopernicusModelAtlas.git#acc --source-secret github-connection --name frontend --context-dir frontend --env-file=../frontend/.env
+oc new-app registry.access.redhat.com/ubi8/nodejs-16~git@github.com:moose-c/CopernicusModelAtlas.git#acc --source-secret github-connection --name frontend --context-dir frontend --env-file=./secrets/.env-frontend
