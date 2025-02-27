ec91d83b916db8b0427bc4ee16659d41704646d1
Moos Castelijn <mooscastelijn@gmail.com>
2025-02-27 12:39:40 +0100

    new build

--- frontend/src/util/authConfig.js
+++ frontend/src/util/authConfig.js
@@ -2,12 +2,14 @@ import { UserManager } from "oidc-client-ts";
 
 const domain = import.meta.env.VITE_APP_AUTH_DOMAIN;
 const clientId = import.meta.env.VITE_APP_AUTH_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_APP_AUTH_CLIENT_SECRET;
 const redirectUri = import.meta.env.VITE_APP_AUTH_CALLBACK_URL;
 const logoutUri = import.meta.env.VITE_APP_AUTH_LOGOUT_URL;
 
 const oidcConfig = {
     authority: domain, // Replace with your Auth0 domain
     client_id: clientId,
+    client_secret: clientSecret,
     redirect_uri: redirectUri,
     response_type: "code",
     scope: "openid profile email",
