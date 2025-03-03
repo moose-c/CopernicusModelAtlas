856675a66cb2c8da63d469425d2e27ef3a3927d0
Moos Castelijn <mooscastelijn@gmail.com>
2025-03-03 09:27:15 +0100

    new build

--- frontend/src/util/authConfig.js
+++ frontend/src/util/authConfig.js
@@ -7,14 +7,13 @@ const redirectUri = import.meta.env.VITE_APP_AUTH_CALLBACK_URL;
 const logoutUri = import.meta.env.VITE_APP_AUTH_LOGOUT_URL;
 
 const oidcConfig = {
-    authority: domain, // Replace with your Auth0 domain
+    authority: domain,
     client_id: clientId,
     client_secret: clientSecret,
     redirect_uri: redirectUri,
     response_type: "code",
     scope: "openid profile email",
     logout_redirect_uri: logoutUri,
-    post_logout_redirect_uri: logoutUri,
 };
 
 export const userManager = new UserManager(oidcConfig);
