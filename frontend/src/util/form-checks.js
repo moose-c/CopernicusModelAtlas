370bbacc41c9d57ae34e95dd6308081236bed208
Moos Castelijn <mooscastelijn@gmail.com>
2025-03-11 11:40:22 +0100

    new build

--- frontend/src/util/form-checks.js
+++ frontend/src/util/form-checks.js
@@ -51,7 +51,7 @@ const checkUploadedFile = async (file, name) => {
     const type = name.split('.')[1]
     if (['csv', 'xlsx'].includes(type)) {
         result = await checkSheet(file)
-    } else if (['.png'].includes(type)) {
+    } else if (['png'].includes(type)) {
         result = true
     }
     return result
