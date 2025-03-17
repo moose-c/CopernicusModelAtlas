399e2d3e6d1f20cce27548ee0874b34de4eb5693
Moos Castelijn <mooscastelijn@gmail.com>
2025-03-17 15:07:02 +0100

    new build

--- frontend/src/util/authConfig.js
+++ frontend/src/util/authConfig.js
@@ -4,7 +4,7 @@ const domain = import.meta.env.VITE_APP_AUTH_DOMAIN;
 const clientId = import.meta.env.VITE_APP_AUTH_CLIENT_ID;
 const clientSecret = import.meta.env.VITE_APP_AUTH_CLIENT_SECRET;
 const redirectUri = import.meta.env.VITE_APP_AUTH_CALLBACK_URL;
-const logoutUri = import.meta.env.VITE_APP_AUTH_LOGOUT_URL;
+const logoutUri = import.meta.env.VITE_APP_AUTH_POST_LOGOUT_URL;
 
 const oidcConfig = {
     authority: domain,
